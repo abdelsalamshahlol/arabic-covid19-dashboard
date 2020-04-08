c296a36f217a94c21db03f5b0c0e414f56267af6
Abdelsalam Shahlol <abdulsalamshahlol@gmail.com>
2020-04-08 20:07:29 +0200

    updated titles

--- src/routes.js
+++ src/routes.js
@@ -4,7 +4,7 @@ const Dashboard = React.lazy(() => import('./views/Dashboard'));
 
 // https://github.com/ReactTraining/react-router/tree/master/packages/react-router-config
 const routes = [
-  {path: '/dashboard', name: 'لوحة المعلومات covid-19', component: Dashboard},
+  {path: '/dashboard', name: 'لوحة معلومات جائحة كورونا المستجد | covid-19', component: Dashboard},
 ];
 
 export default routes;
